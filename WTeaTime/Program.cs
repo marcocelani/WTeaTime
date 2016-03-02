d420d42745fed684b2e3e1ea63cd046b817567c1
marcocelani <marco.celani@hotmail.it>
2016-03-02 16:18:11 +0100

    Only one instance at a time.

--- WTeaTime/Program.cs
+++ WTeaTime/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,25 @@ namespace WTeaTime
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(true, "WTeaTime_{d2b33fa2-df60-4a0d-89bd-504cd7ba65a7}");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new StartForm());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new StartForm());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("Another instance is running...");
+            }
         }
     }
 }
